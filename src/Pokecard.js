fc02b49c107cdf7e8976530db8ba4035fd645ff8
Himadri Bhargava <himadribhargava@gmail.com>
2019-07-06 22:51:47 +0530

    remove default react-app files

---
+++ src/Pokecard.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import "./Pokecard.css";
+import leftPad from 'left-pad';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+class Pokecard extends Component{
+    render(){
+        // defaultProps = pokedata;
+        let {poke} = this.props;
+        let id = leftPad(poke.id, 3, 0);
+        return (
+            <div className="Pokecard">
+                <ul className="pokecard-item">
+                    <li className="pokecard-name">{poke.name}</li>
+                    <img className="pokecard-img" src={`${POKE_API}${id}.png`} />
+                    <li>Type: {poke.type}</li>
+                    <li>EXP: {poke.base_experience}</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Pokecard;
