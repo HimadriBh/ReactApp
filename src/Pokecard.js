6ef691368e578cebbb006107d7350a3f538f2e9f
Himadri Bhargava <himadribhargava@gmail.com>
2019-07-06 23:09:25 +0530

    refactor img tag

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,16 +2,19 @@ import React, {Component} from 'react';
 import "./Pokecard.css";
 import leftPad from 'left-pad';
 
+const POKE_API =  'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
 class Pokecard extends Component{
     render(){
         // defaultProps = pokedata;
         let {poke} = this.props;
         let id = leftPad(poke.id, 3, 0);
+        let imgSrc = `${POKE_API}${id}.png`;
         return (
             <div className="Pokecard">
                 <ul className="pokecard-item">
                     <li className="pokecard-name">{poke.name}</li>
-                    <img className="pokecard-img" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`} />
+                    <img className="pokecard-img" src={imgSrc} alt={poke.name}/>
                     <li>Type: {poke.type}</li>
                     <li>EXP: {poke.base_experience}</li>
                 </ul>
