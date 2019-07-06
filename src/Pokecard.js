25d4c68b4065fe03fe98e78123725c1ac59b24be
Himadri Bhargava <himadribhargava@gmail.com>
2019-07-06 23:47:34 +0530

    simplify variables in Pokecard.js

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -6,17 +6,16 @@ const POKE_API =  'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 class Pokecard extends Component{
     render(){
-        // defaultProps = pokedata;
-        let {poke} = this.props;
-        let id = leftPad(poke.id, 3, 0);
-        let imgSrc = `${POKE_API}${id}.png`;
+        let {id, name, type, exp} = this.props;
+        let imgId = leftPad(id, 3, 0);
+        let imgSrc = `${POKE_API}${imgId}.png`;
         return (
             <div className="Pokecard">
                 <ul className="pokecard-item">
-                    <li className="pokecard-name">{poke.name}</li>
-                    <img className="pokecard-img" src={imgSrc} alt={poke.name}/>
-                    <li>Type: {poke.type}</li>
-                    <li>EXP: {poke.base_experience}</li>
+                    <li className="pokecard-name">{name}</li>
+                    <img className="pokecard-img" src={imgSrc} alt={name}/>
+                    <li>Type: {type}</li>
+                    <li>EXP: {exp}</li>
                 </ul>
             </div>
         );
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -5,8 +5,8 @@ import './Pokedex.css';
 
 
 class Pokedex extends Component{
-    render(){
-        let pokedata = [
+    static defaultProps = {
+        pokemon: [
             {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
             {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
             {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
@@ -15,13 +15,15 @@ class Pokedex extends Component{
             {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
             {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
             {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-        ];
+        ]
+    }
+    render(){
+        const {pokemon} = this.props;
         return (
             <div className="Pokedex">
                 <h1 className="pokedex-title">Pokedex</h1>
-                {pokedata.map(pokemon =>
-                    <Pokecard 
-                    poke={pokemon}/>
+                {pokemon.map(p =>
+                    <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience}/>
                     )}
             </div>
         )
